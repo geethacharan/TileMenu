06e861c9ff79999bf5d06a2b23dd5a2d3f2d0751
charan0613 <charan0613@gmail.com>
2019-08-30 11:42:15 -0700

    Update CommunityActionTileMenuController.js

--- CommunityActionTileMenuController.js
+++ CommunityActionTileMenuController.js
@@ -1,4 +1,23 @@
 ({
+    doInit : function(component, event, helper) {
+        var tilesMenu = [];
+        var obj = {};
+        obj.tileLabel = 'Member Maintenace';
+        obj.communityPage = 'membermaintenance';
+        obj.iconName = 'standard:service_crew';
+        tilesMenu.push(obj);
+        var obj2 = {};
+        obj2.tileLabel = 'Pay Bills';
+        obj2.communityPage = 'billpay';
+        obj2.iconName = 'standard:currency';
+        tilesMenu.push(obj2);
+        var obj3 = {};
+        obj3.tileLabel = 'Policy Documents';
+        obj3.communityPage = 'policydocuments';
+        obj3.iconName = 'standard:service_crew';
+        tilesMenu.push(obj3);
+        component.set("v.tilesMenu",tilesMenu);
+	},
 	handleClick : function(component, event, helper) {
         var id = event.target.parentNode.dataset.menuItemId;
 		console.log('working so far');
@@ -15,5 +34,5 @@
                 pageName: id 
             }
         });
-	}
+	},
 })
