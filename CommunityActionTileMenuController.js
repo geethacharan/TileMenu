6de22385e0022fab7822597c87eea7adc365c01e
charan0613 <charan0613@gmail.com>
2019-08-30 11:43:12 -0700

    Update CommunityActionTileMenuController.js

--- CommunityActionTileMenuController.js
+++ CommunityActionTileMenuController.js
@@ -17,22 +17,22 @@
         obj3.iconName = 'standard:service_crew';
         tilesMenu.push(obj3);
         component.set("v.tilesMenu",tilesMenu);
-	},
-	handleClick : function(component, event, helper) {
-        var id = event.target.parentNode.dataset.menuItemId;
+    },
+    handleClick : function(component, event, helper) {
+	var id = event.target.parentNode.dataset.menuItemId;
 		console.log('working so far');
-        console.log(id);
-        /*var urlEvent = $A.get("e.force:navigateToURL");
-        urlEvent.setParams({
-          "url": id,
-            "isredirect":true
-        });
-        urlEvent.fire();*/
-        component.find("navService").navigate({
-            type: "comm__namedPage",
-            attributes: {
-                pageName: id 
-            }
-        });
-	},
+	console.log(id);
+	/*var urlEvent = $A.get("e.force:navigateToURL");
+	urlEvent.setParams({
+	  "url": id,
+	    "isredirect":true
+	});
+	urlEvent.fire();*/
+	component.find("navService").navigate({
+	    type: "comm__namedPage",
+	    attributes: {
+		pageName: id 
+	    }
+	});
+     },
 })
